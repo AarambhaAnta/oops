c418cd298d1cc24aae4836a96363d1172fcb9967
AarambhaAnta <aarambha108@gmail.com>
2025-07-08 22:40:52 +0530

    learn(friend-function): implement friend function & class

--- 01_basics/friend_function.cpp
+++ 01_basics/friend_function.cpp
@@ -10,8 +10,66 @@ using namespace std;
 // - Friend class that can access private members
 // - Demonstrate friend functionality
 
-int main()
-{
+class RectangleHelper;
+
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(const double& w = 0.0, const double& h = 0.0) : width(w), height(h) {}
+    ~Rectangle() {}
+
+    // getters
+    double getWidth() const {
+        return width;
+    }
+    double getHeight() const {
+        return height;
+    }
+
+    // friend zone
+    friend double calculateArea(const Rectangle& rect);
+    friend RectangleHelper;
+    friend void displayInfo(const Rectangle& rect);
+};
+
+double calculateArea(const Rectangle& rect) {
+    return rect.width * rect.height;
+}
+
+class RectangleHelper {
+private:
+public:
+    double calculatePerimeter(const Rectangle& rect) {
+        return 2 * (rect.width + rect.height);
+    }
+    void scale(Rectangle& rect, const double& fact) {
+        rect.width *= fact;
+        rect.height *= fact;
+    }
+};
+
+void displayInfo(const Rectangle& rect) {
+    cout << "\n===Rectangle===" << endl;
+    cout << "Height: " << rect.getHeight() << endl;
+    cout << "Width: " << rect.getWidth() << endl;
+    cout << "Area: " << calculateArea(rect) << endl;
+    RectangleHelper helper;
+    cout << "Perimeter: " << helper.calculatePerimeter(rect) << endl;
+}
+
+int main() {
     // TODO: Use friend functions and classes
+
+    Rectangle rect1;
+    displayInfo(rect1);
+
+    Rectangle rect2(5.0,3.4);
+    displayInfo(rect2);
+
+    
+
     return 0;
 }
