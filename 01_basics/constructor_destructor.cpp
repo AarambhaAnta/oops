e341a18ba1b066c160b73bf8b9b055afdda9ff24
AarambhaAnta <aarambha108@gmail.com>
2025-07-08 20:35:29 +0530

    learn(constrctor-destructor): add constructor and destructor and getter functions and display info function

--- 01_basics/constructor_destructor.cpp
+++ 01_basics/constructor_destructor.cpp
@@ -17,11 +17,62 @@ private:
     string accountNumber;
     string accountHolder;
     double balance;
-public:
 
+public:
+    // Default constructor
+    BankAccount() : accountNumber(""), accountHolder(""), balance(0.0) {
+        cout << "Object created with default constructor." << endl;
+    }
+
+    // Parameterized constructor
+    BankAccount(string acNum, string acHold, double balance)
+        : accountNumber(acNum), accountHolder(acHold), balance(balance) {
+        cout << "Object created: " << accountNumber << endl;
+    }
+
+    // Copy constructor
+    BankAccount(const BankAccount& other)
+        : accountNumber(other.accountNumber), accountHolder(other.accountHolder),
+          balance(other.balance) {
+        cout << "Object created with copy constructor" << endl;
+    }
+
+    // Destructor
+    ~BankAccount() {
+        cout << "Object destroyed: " << accountNumber << endl;
+    }
+
+    // Getters
+    string getAccountNumber() const {
+        return accountNumber;
+    }
+    string getAccountHolder() const {
+        return accountHolder;
+    }
+    double getBalance() const {
+        return balance;
+    }
+
+    // Display Info
+    void displayInfo() const {
+        cout << "\n===Account details===" << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Account Holder: " << accountHolder << endl;
+        cout << "Account Balance: " << balance << endl;
+    }
 };
 
 int main() {
     // TODO: Create objects using different constructors
+
+    BankAccount account1;
+    account1.displayInfo();
+
+    BankAccount account2("1234", "Aditya Kumar", 50000000.0);
+    account2.displayInfo();
+
+    BankAccount account3 = account2;
+    account3.displayInfo();
+
     return 0;
 }
