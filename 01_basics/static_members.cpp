ecaaa41b7b5f90abb8ff85a49f0f22c3b4b1a650
AarambhaAnta <aarambha108@gmail.com>
2025-07-09 06:00:30 +0530

    learn(static-members): implement static member and static member function along with some getter and utility functions

--- 01_basics/static_members.cpp
+++ 01_basics/static_members.cpp
@@ -16,14 +16,46 @@ private:
     int value;
 
 public:
-    Counter(const int& val = 0) : value(val) {}
+    Counter(const int& val = 0) : value(val) {
+        ++objCount;
+    }
     ~Counter() {};
 
     // getters
-
+    static int getTotalCount() {
+        return objCount;
+    }
+    int getValue() const {
+        return value;
+    }
     // setters
+
+    // utility
+    void increment() {
+        ++value;
+    }
+    void decrement() {
+        --value;
+    }
 };
+
+int Counter::objCount = 0;
+
 int main() {
     // TODO: Create multiple objects and show static behavior
+
+    Counter c1;
+    Counter c2(202);
+    cout << "\nCounter 1 value: " << c1.getValue() << endl;
+    cout << "Counter 2 value: " << c2.getValue() << endl;
+    cout << "Total Object Count: " << Counter::getTotalCount() << endl;
+
+    Counter c3;
+    c3.increment();
+    c3.increment();
+    cout << "\nCounter 3 value: " << c3.getValue() << endl;
+    c3.decrement();
+    cout << "Counter 3 value: " << c3.getValue() << endl;
+    cout << "Total Object Count: " << Counter::getTotalCount() << endl;
     return 0;
 }
