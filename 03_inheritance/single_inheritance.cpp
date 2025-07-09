e752ab1bb419e9c0e6df5cf6e86065dac9399854
AarambhaAnta <aarambha108@gmail.com>
2025-07-09 21:51:56 +0530

    learn(inheritance): implement class structure for Vehicle class with a publically drived class Car

--- 03_inheritance/single_inheritance.cpp
+++ 03_inheritance/single_inheritance.cpp
@@ -12,8 +12,38 @@ using namespace std;
 // - Show constructor chaining
 // - Access base class members
 
-int main()
-{
+class Vehicle {
+protected:
+    string brand;
+    string model;
+    int year;
+
+public:
+    // Constructor
+
+    // Getters
+
+    // Setters
+
+    // Utility
+};
+
+class Car : public Vehicle {
+protected:
+    int doors;
+    string fuelType;
+
+public:
+    // Constructor
+
+    // Getters
+
+    // Setters
+
+    // Utility
+};
+
+int main() {
     // TODO: Create objects and show inheritance
     return 0;
 }
