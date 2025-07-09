55c5bab6702825a5d1ab9d06278806954e737ecd
AarambhaAnta <aarambha108@gmail.com>
2025-07-09 23:02:02 +0530

    learn(single-inheritance): implement single inheritance with Vehicle(parent) & Car(child)
    - implement Access control for data members and class inheritance
    - implement constructor chaining
    - implement method inheritance using virtual methods
    - implement dynamic cast to cast parent point into child to access child specific methods

--- 03_inheritance/single_inheritance.cpp
+++ 03_inheritance/single_inheritance.cpp
@@ -17,15 +17,37 @@ protected:
     string brand;
     string model;
     int year;
+    const int curYear = 2025;
 
 public:
     // Constructor
+    Vehicle(const string& b = "", const string& m = "", const int& y = 0)
+        : brand(b), model(m), year(y) {}
+    ~Vehicle() {}
 
     // Getters
+    string getBrand() const {
+        return brand;
+    }
+    string getModel() const {
+        return model;
+    }
+    int getYear() const {
+        return year;
+    }
+    int getAge() const {
+        return curYear - year;
+    }
 
     // Setters
 
     // Utility
+    virtual void displayInfo() const {
+        cout << "\n===Vehicle Info===" << endl;
+        cout << "Brand: " << brand << endl;
+        cout << "Model: " << model << endl;
+        cout << "Year: " << year << endl;
+    }
 };
 
 class Car : public Vehicle {
@@ -35,15 +57,51 @@ protected:
 
 public:
     // Constructor
+    Car(const string& b = "Toyota", const string& m = "", const int& y = 0, const int& d = 4,
+        const string& f = "")
+        : Vehicle(b, m, y), doors(d), fuelType(f) {}
+    ~Car() {}
 
     // Getters
-
+    int getDoors() const {
+        return doors;
+    }
+    string getFuelType() const {
+        return fuelType;
+    }
     // Setters
 
     // Utility
+    void displayInfo() const override {
+        Vehicle::displayInfo();
+        cout << "===Car Info===" << endl;
+        cout << "Doors: " << doors << endl;
+        cout << "Fuel Type: " << fuelType << endl;
+    }
 };
 
 int main() {
     // TODO: Create objects and show inheritance
+
+    cout << "\n===Single Inheritance Testing===" << endl;
+    Car c1;
+    Car c2("Rolls Royce", "Phantom", 2024, 5, "Hybrid");
+    c1.displayInfo();
+    c2.displayInfo();
+
+    Vehicle* vehiclePrt = &c2;
+    vehiclePrt->displayInfo();
+    cout << "\n===Parent Class Pointer Access===" << endl;
+    cout << "Brand: " << vehiclePrt->getBrand() << endl;
+
+    // `Vehicle*` pointer can only access the methods of `Vehicle` class
+    // to access the `Car` class methods use `dynamic_cast<Car*>(vehiclePtr)`
+    // cout << "Fuel Type: " << vehiclePrt.getFuelType() << endl;
+
+    Car* c3 = dynamic_cast<Car*>(vehiclePrt);
+    cout << "\n===Dynamic Cast===" << endl;
+    cout << "Brand: " << c3->getBrand() << endl;
+    cout << "Fuel Type: " << c3->getFuelType() << endl;
+
     return 0;
 }
