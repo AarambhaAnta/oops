a5199fdd08984e0a7b206451416eb0b8fbf171e1
AarambhaAnta <aarambha108@gmail.com>
2025-07-09 08:19:16 +0530

    learn(data-hiding): implement encapsulation
    - add private, protected, and public members
    - add getter and setters to access and modify data
    - add validation for setters
    - add displayInfo() utility to display data of object

--- 02_encapsulation/data_hiding.cpp
+++ 02_encapsulation/data_hiding.cpp
@@ -12,7 +12,7 @@ using namespace std;
 // - Validate data in setters
 // - Demonstrate data hiding principles
 
-class Empolyee {
+class Employee {
 private:
     double salary;
     int id;
@@ -24,13 +24,47 @@ public:
     string name;
 
     // Constructor & destructor
-
+    Employee(const string& name = "", const int& id = 0, const string& dep = "",
+             const double& salary = 0.0)
+        : salary(salary), id(id), department(dep), name(name) {
+        cout << "===Object created===" << endl;
+        cout << "ID: " << id << endl;
+    }
+    ~Employee() {}
     // Getters & Setters;
-
+    int getId() const {
+        return id;
+    }
+    double getSalary() const {
+        return salary;
+    }
+    string getDepartment() const {
+        return department;
+    }
+    void setDepartment(const string& dep) {
+        this->department = dep;
+    }
+    void setSalary(const double& salary) {
+        if (salary > 0.0)
+            this->salary = salary;
+    }
     // Utilities
+    void displayInfo() const {
+        cout << "\n===Employee Info===" << endl;
+        cout << "Id: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Department: " << department << endl;
+        cout << "Salary: $" << salary << endl;
+    }
 };
 
 int main() {
     // TODO: Show how encapsulation protects data
+
+    Employee e1;
+    Employee e2("John Smith", 80005, "hardware", 500.55);
+    e1.displayInfo();
+    e2.displayInfo();
+
     return 0;
 }
