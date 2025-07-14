d3afd0d3e2100d3f95e3e4810c82c896c59f5ac3
AarambhaAnta <aarambha108@gmail.com>
2025-07-14 23:31:43 +0530

    solve(diamond-problem): implement diamond problem with Person, Student, Teacher, and TeachingAssistant class

--- 03_inheritance/diamond_problem.cpp
+++ 03_inheritance/diamond_problem.cpp
@@ -24,19 +24,44 @@ public:
     virtual ~Person() {
         cout << "Person Destructor..." << endl;
     }
+
+    // Getters
+    string getName() const {
+        return name;
+    }
+    int getAge() const {
+        return age;
+    }
+
+    virtual void displayInfo() const {
+        cout << "<--Person Info-->" << endl;
+        cout << "Person Name: " << name << endl;
+        cout << "Person Age: " << age << endl;
+    }
 };
 
 class Student : public Person {
 private:
-    int studentId;
+    string studentId;
 
 public:
-    Student(string name = "", int age = 0, int studentId = 0)
+    Student(string name = "", int age = 0, string studentId = 0)
         : Person(name, age), studentId(studentId) {
         cout << "Student Constructro..." << endl;
     }
     virtual ~Student() {
         cout << "Student Destructor..." << endl;
+    }
+
+    // Getter
+    string getStudentId() const {
+        return studentId;
+    }
+
+    void displayInfo() const override {
+        Person::displayInfo();
+        cout << "\n<--Student Info-->" << endl;
+        cout << "Student Id: " << studentId << endl;
     }
 };
 
@@ -52,11 +77,44 @@ public:
     virtual ~Teacher() {
         cout << "Teacher Destructor..." << endl;
     }
+
+    // Getter
+    string getSubject() const {
+        return subject;
+    }
+
+    void displayInfo() const override {
+        Person::displayInfo();
+        cout << "\n<--Teacher Info-->" << endl;
+        cout << "Teacher Subject: " << subject << endl;
+    }
 };
 
-class TeachingAssistant {};
+class TeachingAssistant : public Student, public Teacher {
+public:
+    TeachingAssistant(string name = "", int age = 0, string studentId = "", string subject = "")
+        : Student(name, age, studentId), Teacher(name, age, subject) {
+        cout << "TeachingAssistant Constructor..." << endl;
+    }
+    ~TeachingAssistant() {
+        cout << "TeachingAssistant Desturctor..." << endl;
+    }
+
+    void displayInfo() const override {
+        Student::displayInfo();
+        Teacher::displayInfo();
+    }
+};
 
 int main() {
     // TODO: Demonstrate diamond problem and solution
+
+    cout << "\n===Diamond Problem Demonstration===\n" << endl;
+
+    TeachingAssistant ta("Alice", 25, "ST001", "Computer Science");
+    // cout << "Name: " << ta.getName() << endl;  // Compilation erro due to ambiguity
+    cout << "\nName: " << ta.Student::getName() << endl;
+    ta.displayInfo();
+
     return 0;
 }
