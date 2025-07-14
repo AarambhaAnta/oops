e18bf91397afee7355492e472d88234ed2c66259
AarambhaAnta <aarambha108@gmail.com>
2025-07-14 23:08:15 +0530

    solve(inheritance): implement the structure for diamond problem using 4 classes

--- 03_inheritance/diamond_problem.cpp
+++ 03_inheritance/diamond_problem.cpp
@@ -12,8 +12,51 @@ using namespace std;
 // - Show diamond problem
 // - Solve using virtual inheritance
 
-int main()
-{
+class Person {
+private:
+    string name;
+    int age;
+
+public:
+    Person(string name = "", int age = 0) : name(name), age(age) {
+        cout << "Person Constructor..." << endl;
+    }
+    virtual ~Person() {
+        cout << "Person Destructor..." << endl;
+    }
+};
+
+class Student : public Person {
+private:
+    int studentId;
+
+public:
+    Student(string name = "", int age = 0, int studentId = 0)
+        : Person(name, age), studentId(studentId) {
+        cout << "Student Constructro..." << endl;
+    }
+    virtual ~Student() {
+        cout << "Student Destructor..." << endl;
+    }
+};
+
+class Teacher : public Person {
+private:
+    string subject;
+
+public:
+    Teacher(string name = "", int age = 0, string subject = "")
+        : Person(name, age), subject(subject) {
+        cout << "Teacher Constructor..." << endl;
+    }
+    virtual ~Teacher() {
+        cout << "Teacher Destructor..." << endl;
+    }
+};
+
+class TeachingAssistant {};
+
+int main() {
     // TODO: Demonstrate diamond problem and solution
     return 0;
 }
